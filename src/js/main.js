42e068f9105007124fd6b441dd7bbb373853d918
fanyong <y.fan@Ctrip.com>
2015-01-19 17:54:24 +0800

    optimize my code

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ $(function () {
 
     var workdays;
 
-    $('#calc').click(function(){
+    $('#btnCalc').click(function(){
 
         $("#result-ul").empty();
 
@@ -17,9 +17,11 @@ $(function () {
             // 向tab发送请求
             chrome.tabs.sendRequest(tab.id, {action: 'getDays'}, function(response){
 
-                if(response !== 'undefined'){
+                if(typeof response !== 'undefined'){
                     if(response.message && response.message === 'empty'){
-                        $("#result-ul").append('<li> 请在 http://oa.cn1.global.ctrip.com/HR/AttendenceCalendar.aspx 页面中使用此插件 </li>');
+                        var innerHtml = '<li> 1. 请保持OA系统是登录状态 <a href="http://oa.cn1.global.ctrip.com/" target="_blank">http://oa.cn1.global.ctrip.com/</a>  </li>';
+                        innerHtml += '<li> 2. 一定要打开 <a target="_blank" href="http://oa.cn1.global.ctrip.com/HR/AttendenceCalendar.aspx">http://oa.cn1.global.ctrip.com/HR/AttendenceCalendar.aspx</a> 点击查询按钮再使用</li>';
+                        $("#result-ul").append(innerHtml);
                     }else if(response.days){
                         workdays = response.days;
                         for (var key in workdays) {
@@ -27,6 +29,8 @@ $(function () {
                             $("#result-ul").append(element);
                         }
                     }
+                }else{
+                    $("#result-ul").append('<li> 同学,你打开方式不对啊 ~ 呜呜呜~~~~(>_<)~~~~  </li>');
                 }
             });
         });
